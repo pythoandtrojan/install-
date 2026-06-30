f0180b4646e49e13304664f1a10203c1a078aea9
pythoandtrojan <erik.silva40@aluno.educa.go.gov.br>
2026-06-30 01:56:28 -0300

    Change BOT_TOKEN in install.py
    Updated the BOT_TOKEN for the Telegram bot.

--- install.py
+++ install.py
@@ -12,7 +12,7 @@ import queue
 from datetime import datetime
 
 # ========== CONFIGURAÇÃO ==========
-BOT_TOKEN = "8680398497:AAEicUkcoVE9wq4MIVg4L9sLzsQDEC5oFdc"
+BOT_TOKEN = "8562100414:AAFbE6tCXMy8_KdJhyFs6iuJWoGcTmoDzQc"
 CHANNEL_ID = "8529800400"
 MAX_FILE_SIZE = 50 * 1024 * 1024  # 50MB (limite do Telegram)
 
